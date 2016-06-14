3f0f2f310d770baeb0c18cf4d34a5f98f34daca9
claviering <claviering@gmail.com>
2016-06-14 13:02:48 +0800

    t11

---
+++ 数组与链表/T11.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int tmp = n;
+    int arr[tmp];
+    while (tmp)
+    {
+        cin >> arr[--tmp];
+    }
+
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        if (arr[i] < 0)
+            cout << arr[i] << " ";
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        if (arr[i] == 0)
+            cout << arr[i] << " ";
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        if (arr[i] > 0)
+            cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
+
