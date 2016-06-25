64bc2c62e86ee354e2a87f1aaca1051e6325cbbf
weiye <claviering@gmail.com>
2016-06-25 21:54:28 +0800

    use other code

--- 递归/2734.cpp
+++ 递归/2734.cpp
@@ -1,47 +1,48 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<math.h>
-#include<iostream>
-#include<algorithm>
-#include<queue>
-#include<stack>
-#include<vector>
-#include<map>
-#include<set>
-using namespace std;
-int c1[10005], c2[10005];
-
-int main()
-{
-	int n,m;
-	int i, j, k,t=0; 
-	int value[100],num[100];
-	while(scanf("%d%d",&n,&m)!=EOF)
-	{
-		if(t==0) t=1;
-		else cout<<endl;
-		for(i=1;i<=m;i++)
-		{scanf("%d%d",&value[i],&num[i]);}
-		memset(c1,0,sizeof(c1));
-		memset(c2,0,sizeof(c2));
-		for(i=0; i<=num[1]*value[1]; i+=value[1])
-		{
-			c1[i] = 1;
-		}
-		for(i=2; i<=m; i++)
-		{
-			for(j=0; j<=n; j++)
-			for(k=0; k<=value[i]*num[i]; k+=value[i])
-			{
-				c2[j+k] += c1[j];
-			}
-			for(j=0; j<=n; j++)
-			{
-				c1[j] = c2[j];
-				c2[j] = 0;
-			}
-		}
-		printf("%d\n",c1[n]);
-	}
+#include <stdio.h>  
+#include <stdlib.h>  
+#include <iostream>  
+#include <string.h>  
+using namespace std;  
+int sum,ways;  
+int num[1010];  
+int value,n;  
+void DFS(int x)  
+{  
+    if( sum == value )  
+    {  
+        ways++;  
+        return ;  
+    }  
+    for(int i=x; i<=value; i++)  
+    {  
+        if( num[i] && sum + i <= value )  
+        {  
+            num[i]--;  
+            sum += i;  
+            DFS(i);  
+            num[i]++;  
+            sum -= i;  
+        }  
+    }  
+}  
+int main(void)  
+{  
+    int x,y;  
+    int pe = 0;  
+    while( cin >> value >> n )  
+    {  
+        if( pe )  
+            cout << endl;  
+        pe = 1;  
+        memset(num,0,sizeof(num));  
+        sum = ways = 0;  
+        for(int i=0; i<n; i++)  
+        {  
+            cin >> x >> y;  
+            num[x] = y;  
+        }  
+        DFS(1);  
+        cout << ways << endl;  
+    }  
+return 0;  
 }
